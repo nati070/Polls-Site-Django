05743f300ed53989ce09b2eedd3867d2bb90f282
Nati <nnati070@gmail.com>
2021-04-30 17:25:02 +0300

    init

--- polls/models.py
+++ polls/models.py
@@ -18,7 +18,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
 class Choice(models.Model):
-    question = models.ForeignKey   
+    question = models.ForeignKey(Question , on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
 
