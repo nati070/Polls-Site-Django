673148a437f08145249f60f496a259a35d911ed9
Nati <nnati070@gmail.com>
2021-04-30 18:14:25 +0300

    fix view class

--- polls/views.py
+++ polls/views.py
@@ -26,12 +26,12 @@ def detail(request , question_id):
     return render(request , 'polls/detail.html' , {"question" : question})
 def results(request , question_id):
     question = get_object_or_404(Question , pk = question_id)
-    return render(request , 'polls/results.html' , 'question' : question)
+    return render(request , 'polls/results.html' , {'question' : question})
 
 def vote(request , question_id):
     question  = get_object_or_404(Question , pk = question_id)
     try: 
-        selected_choice = question.choise_set.get(pk = request.POST['choice'])
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
     except (KeyError , Choice.DoesNotExist):
         return render(request, 'poll/detail.html' , {'question' : question, 'error_message' : "You didn't select a choice.",})
     else:
