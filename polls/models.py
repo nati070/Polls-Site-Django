9080488657349dfbc1a7e47d4de71c1053818f9e
Nati <nnati070@gmail.com>
2021-05-05 19:02:07 +0300

    cahnge the model view inside the administration

--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,13 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
 
 class Question(models.Model):
+
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
 
@@ -14,8 +16,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question , on_delete = models.CASCADE)
